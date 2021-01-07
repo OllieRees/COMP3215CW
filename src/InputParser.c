5a52f732a2ddc121b23f281fb14c8743f11a0996
Ollie Rees <ollie.rees@me.com>
2021-01-07 09:08:00 +0000

    Reformed InputParser to fix issues where it was assuming the key was numtask not numtasks, and segfault in taskline parsing.

--- src/InputParser.c
+++ src/InputParser.c
@@ -1,25 +1,37 @@
 #include "InputParser.h"
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAXCHAR 256
 
 uint8_t parseTaskCountLine(char * taskCountLine) {
-    taskCountLine += 7; //Skip NumTask word
+    taskCountLine += 8; //Skip NumTasks word
     for(; *taskCountLine == ' ' || *taskCountLine == '\t'; taskCountLine++); //Skip spaces
     return atoi(taskCountLine);
 }
 
 Task * parseTaskLine(char * taskLine) {
-    char * name = strtok(taskLine, " "); //first word is the name
-    uint8_t exec_time = (uint8_t) atoi(strtok(NULL, " ")); //second word is the execution time 
-    uint8_t period = (uint8_t) atoi(strtok(NULL, " ")); //third word is the period
-    return createTask(name, exec_time, period);
+    Task * task = malloc(sizeof(Task));
+    //split into words
+    //first word is the name
+    //second word is the execution time
+    //third word is the period
+   
+    int taskLineLen = strlen(taskLine);
+    char * taskLineCpy = calloc(taskLineLen + 1, sizeof(char));
+    strncpy(taskLineCpy, taskLine, taskLineLen);
+    
+    task -> name = strtok(taskLineCpy, " ");
+    task -> exec_time = atoi(strtok(NULL, " "));
+    task -> period = atoi(strtok(NULL, " "));
+    return task;
 }
 
 Task ** parseInputFile(char * fileLoc, uint8_t * taskCountPtr) {
     //Read File
     FILE * fp = fopen(fileLoc, "r"); 
-    
+     
     //Couldn't open File
     if (fp == NULL) {
         printf("Couldn't find the file given by %s\n", fileLoc);
@@ -36,14 +48,14 @@ Task ** parseInputFile(char * fileLoc, uint8_t * taskCountPtr) {
     }
 
     //parse task count 
-    *(taskCountPtr) = parseTaskCountLine(lineBuf);
+    *taskCountPtr = parseTaskCountLine(lineBuf);
 
     //allocate space for taskCount amount of tasks
     Task ** taskList = malloc(sizeof(Task) * *taskCountPtr);
 
     //loop through the task lines
     uint8_t i;
-    for(i = 0; i < *taskCountPtr ; i--) {
+    for(i = 0; i < *taskCountPtr ; i++) {
         //Check line is readable
         if(fgets(lineBuf, MAXCHAR, fp) == NULL) {
             printf("Can't read task line\n");
