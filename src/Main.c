2e1970d00966b594f6bdc2627fef3e8979cdd710
Ollie Rees <ollie.rees@me.com>
2021-01-11 22:39:54 +0000

    Swapped position of supertime duration and output file loc in the command line

--- src/Main.c
+++ src/Main.c
@@ -2,17 +2,14 @@
 #include "Scheduler.h"
 
 int main(int argc, char ** argv) {
+    if (argc < 5) {
+        printf("ERROR: Not enough arguements\n");
+        return 1;
+    }
     uint8_t taskCount;
     Task ** tasks = parseInputFile(argv[1], &taskCount);
-    uint8_t i;
-    for(i = 0; i < taskCount; i++) {
-        printf("%s %d %d\n", tasks[i] -> name, tasks[i] -> exec_time, tasks[i] -> period);
-    }
-     
-    //if argc < 4
-        //printf("ERROR\n");
-    //uint8_t taskCount;
-    //Task ** tasks = parseInputFile(argv[1], &taskCount);
-    //Scheduler * scheduler = createScheduler(tasks, taskCount);
-    //runScheduler(argv[2], argv[3], scheduler);
+    Scheduler * scheduler = createScheduler(tasks, taskCount);
+    SchedulingAlgorithm * priorityFunc = atoi(argv[4]) == 0 ? assignPriority_RMS : assignPriority_EDF;
+    runScheduler(atoi(argv[3]), argv[2], priorityFunc, scheduler);
+    return 0;
 }
